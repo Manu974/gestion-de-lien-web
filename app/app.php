73edd0a26e11c0e04e8f18c84aee228c99a979bb
DIJOUX <emmanuel.dijoux14@gmail.com>
2017-04-09 11:08:22 +0200

    ajout de la gestion des erreurs

--- app/app.php
+++ app/app.php
@@ -68,3 +68,19 @@ $app['dao.link'] = function ($app) {
 };
 
 
+// Register error handler
+$app->error(function (\Exception $e, Request $request, $code) use ($app) {
+    switch ($code) {
+        case 403:
+            $message = 'Access denied.';
+            break;
+        case 404:
+            $message = 'The requested resource could not be found.';
+            break;
+        default:
+            $message = "Something went wrong.";
+    }
+    return $app['twig']->render('error.html.twig', array('message' => $message));
+});
+
+
